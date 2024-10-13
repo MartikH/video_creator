3e254c53a8b8d0240472fbcea08f39a2ba1615b6
MartikH <51113845+MartikH@users.noreply.github.com>
2024-10-13 11:56:55 +0400

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,11 +1,16 @@
 import 'dart:io';
 
-import 'package:file_picker/file_picker.dart'; // Для выбора папки
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
+import 'package:video_creator/styles/colors.dart';
 
-import 'image_audio_picker.dart';
-import 'video_creator.dart';
+import 'helpers/file_picker_helper.dart'; // Ճիշտ ուղի
+import 'styles/button_styles.dart';
+import 'styles/container_styles.dart';
+import 'styles/text_styles.dart';
+import 'widgets/audio_player_widget.dart';
+import 'widgets/video_creator.dart';
+import 'widgets/video_player_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -37,26 +42,24 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   String? imagePath;
   String? audioPath;
-  String? videoPath; // Путь к созданному видео
-  String? videoFileName; // Имя созданного видеофайла
+  String? videoPath;
+  String? videoFileName;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Create Video from Image & Audio',
-            textAlign: TextAlign.center),
+        centerTitle: true,
+        title: Text('Create Video from Image & Audio',
+            style: AppTextStyles.bodyText),
       ),
       body: Center(
         child: ConstrainedBox(
-          constraints: const BoxConstraints(
-            maxWidth: 400, // Ограничиваем ширину колонны до 400 пикселей
-          ),
+          constraints: const BoxConstraints(maxWidth: 400),
           child: Padding(
             padding: const EdgeInsets.all(16.0),
             child: ListView(
               children: [
-                // 1. Кнопка выбора изображения
                 Center(
                   child: ElevatedButton(
                     onPressed: () async {
@@ -67,42 +70,38 @@ class _HomeScreenState extends State<HomeScreen> {
                         });
                       }
                     },
-                    child: const Text('Pick Image'),
+                    style: AppButtonStyles.elevatedButtonStyle,
+                    child: Text('Pick Image', style: AppTextStyles.button),
                   ),
                 ),
                 const SizedBox(height: 16),
-
-                // 2. Контейнер для изображения (появляется только если изображение выбрано)
                 if (imagePath != null)
                   Column(
                     children: [
                       Container(
-                        height: 220,
-                        // Устанавливаем фиксированную высоту для контейнера изображения
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.indigo),
-                        ),
+                        width: double.infinity,
+                        height: 240,
+                        decoration: AppContainerStyles.boxStyle,
                         child: Column(
                           children: [
                             Container(
                               height: 170,
                               child: Image.file(
                                 File(imagePath!),
-                                fit: BoxFit.contain, // Истинный вид изображения
+                                fit: BoxFit.contain,
                               ),
                             ),
-                            SizedBox(height: 5),
+                            const SizedBox(height: 5),
                             Text('$imagePath!',
+                                maxLines: 3,
                                 textAlign: TextAlign.center,
-                                style: TextStyle(fontSize: 10)),
+                                style: AppTextStyles.pathname),
                           ],
                         ),
                       ),
                       const SizedBox(height: 16),
                     ],
                   ),
-
-                // 3. Кнопка выбора аудио
                 Center(
                   child: ElevatedButton(
                     onPressed: () async {
@@ -113,44 +112,40 @@ class _HomeScreenState extends State<HomeScreen> {
                         });
                       }
                     },
-                    child: const Text('Pick Audio'),
+                    style: AppButtonStyles.elevatedButtonStyle,
+                    child: Text('Pick Audio', style: AppTextStyles.button),
                   ),
                 ),
                 const SizedBox(height: 16),
-
-                // 4. Контейнер для аудиоплеера (появляется только если аудио выбрано)
                 if (audioPath != null)
                   Column(
                     children: [
                       Container(
                         height: 100,
-                        // Устанавливаем фиксированную высоту для аудиоплеера
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.indigo),
-                        ),
+                        decoration: AppContainerStyles.boxStyle,
                         child: AudioPlayerWidget(audioPath: audioPath!),
                       ),
                       const SizedBox(height: 16),
                     ],
                   ),
-
-                // 5. Кнопка для создания видео
                 Center(
                   child: ElevatedButton(
                     onPressed: () async {
                       if (imagePath != null && audioPath != null) {
-                        // Показываем SnackBar, что видео создается
                         ScaffoldMessenger.of(context).showSnackBar(
                           const SnackBar(
-                            content: Text('Video is being created, please wait...'),
-                            duration: Duration(seconds: 2), // Длительность отображения
+                            backgroundColor: AppColors.background,
+                            content: Text(
+                              'Video is being created, please wait...',
+                              textAlign: TextAlign.center,
+                            ),
+                            duration: Duration(seconds: 5),
                           ),
                         );
 
-                        // Показать индикатор загрузки
                         showDialog(
                           context: context,
-                          barrierDismissible: false, // Запрещаем закрывать диалог нажатиями вне
+                          barrierDismissible: false,
                           builder: (BuildContext context) {
                             return Dialog(
                               child: Padding(
@@ -160,7 +155,8 @@ class _HomeScreenState extends State<HomeScreen> {
                                   children: [
                                     CircularProgressIndicator(),
                                     const SizedBox(width: 16),
-                                    const Text('Creating Video...'),
+                                    Text('Creating Video...',
+                                        style: AppTextStyles.bodyText),
                                   ],
                                 ),
                               ),
@@ -168,108 +164,111 @@ class _HomeScreenState extends State<HomeScreen> {
                           },
                         );
 
-                        // Создаем видео и получаем его путь
                         videoPath = await VideoCreator.createVideo(
                             imagePath!, audioPath!, context);
 
-                        // Закрываем диалог загрузки
                         Navigator.pop(context);
-
                         setState(() {
-                          // Получаем имя файла из полного пути
                           videoFileName = videoPath!.split('/').last;
                         });
 
-                        // Выводим полный путь к созданному видео
                         if (videoPath != null) {
                           ScaffoldMessenger.of(context).showSnackBar(
                             SnackBar(
-                              content: Text('Created Video: $videoFileName\nPath: $videoPath'),
+                              backgroundColor: AppColors.background,
+                              content: Text(
+                                'Created Video: $videoFileName\nPath: $videoPath',
+                                textAlign: TextAlign.center,
+                              ),
                             ),
                           );
                         }
                       } else {
                         ScaffoldMessenger.of(context).showSnackBar(
                           const SnackBar(
-                            content: Text('Please select both image and audio files!'),
+                            backgroundColor: AppColors.background,
+                            content: Text(
+                              'Please select both image and audio files!',
+                              textAlign: TextAlign.center,
+                            ),
                           ),
                         );
                       }
                     },
-                    child: const Text('Create Video'),
+                    style: AppButtonStyles.elevatedButtonStyle,
+                    child: Text('Create Video', style: AppTextStyles.button),
                   ),
                 ),
                 const SizedBox(height: 16),
-
-                // 6. Контейнер для показа видео (появляется только если видео создано)
                 if (videoPath != null)
                   Column(
                     children: [
                       Container(
                         height: 250,
-                        // Устанавливаем высоту контейнера для видео
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.indigo),
-                        ),
-                        child: VideoPlayerWidget(
-                            videoPath:
-                                videoPath!), // Видео теперь прокручивается
+                        decoration: AppContainerStyles.boxStyle,
+                        child: VideoPlayerWidget(videoPath: videoPath!),
                       ),
                       const SizedBox(height: 16),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: [
-                          // Кнопка для сохранения видео в выбранную папку
                           ElevatedButton(
                             onPressed: () async {
-                              // Позволяем пользователю выбрать папку для сохранения
                               String? selectedPath =
                                   await FilePicker.platform.getDirectoryPath();
                               if (selectedPath != null && videoPath != null) {
-                                // Сохраняем видео в выбранную папку
                                 final File videoFile = File(videoPath!);
                                 final String newVideoPath =
                                     '$selectedPath/$videoFileName';
                                 await videoFile.copy(newVideoPath);
                                 ScaffoldMessenger.of(context).showSnackBar(
                                   SnackBar(
+                                      backgroundColor: AppColors.background,
                                       content: Text(
-                                          'Video saved to: $newVideoPath')),
+                                        'Video saved to: $newVideoPath',
+                                        textAlign: TextAlign.center,
+                                      )),
                                 );
                               }
                             },
-                            child: const Text('Save Video'),
+                            style: AppButtonStyles.elevatedButtonStyle,
+                            child:
+                                Text('Save Video', style: AppTextStyles.button),
                           ),
-                          // Кнопка для удаления видео
                           ElevatedButton(
                             onPressed: () async {
                               if (videoPath != null) {
-                                // Удаляем видео
                                 final File videoFile = File(videoPath!);
                                 if (await videoFile.exists()) {
                                   await videoFile.delete();
                                   setState(() {
-                                    videoPath = null; // Очищаем путь
-                                    videoFileName = null; // Очищаем имя файла
+                                    videoPath = null;
+                                    videoFileName = null;
                                   });
                                   ScaffoldMessenger.of(context).showSnackBar(
                                     const SnackBar(
-                                        content: Text('Video deleted!')),
+                                        backgroundColor: AppColors.background,
+                                        content: Text(
+                                          'Video deleted!',
+                                          textAlign: TextAlign.center,
+                                        )),
                                   );
                                 }
                               }
                             },
-                            child: const Text('Delete Video'),
+                            style: AppButtonStyles.elevatedButtonStyle,
+                            child: Text('Delete Video',
+                                style: AppTextStyles.button),
                           ),
                         ],
                       ),
-                      const SizedBox(height: 8), // Отступ перед текстом
-                      // Текст для отображения имени файла и пути
+                      const SizedBox(height: 8),
                       if (videoFileName != null && videoPath != null)
                         Text(
                           'File: $videoFileName\nPath: $videoPath',
+                          maxLines: 3,
                           textAlign: TextAlign.center,
-                          style: const TextStyle(fontSize: 12), // Размер шрифта
+                          style: AppTextStyles.pathname,
                         ),
                     ],
                   ),
@@ -278,78 +277,6 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
-    );
-  }
-}
-
-// Виджет для воспроизведения видео
-class VideoPlayerWidget extends StatefulWidget {
-  final String videoPath;
-
-  const VideoPlayerWidget({Key? key, required this.videoPath})
-      : super(key: key);
-
-  @override
-  _VideoPlayerWidgetState createState() => _VideoPlayerWidgetState();
-}
-
-class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
-  late VideoPlayerController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = VideoPlayerController.file(File(widget.videoPath))
-      ..initialize().then((_) {
-        setState(() {}); // Обновляем состояние после инициализации
-      });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        if (_controller.value.isInitialized)
-          Container(
-            width: double.infinity,
-            height: 200, // Устанавливаем фиксированную высоту для видео
-            child: AspectRatio(
-              aspectRatio: _controller.value.aspectRatio,
-              child: VideoPlayer(_controller),
-            ),
-          ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            IconButton(
-              icon: Icon(
-                  _controller.value.isPlaying ? Icons.pause : Icons.play_arrow),
-              onPressed: () {
-                setState(() {
-                  _controller.value.isPlaying
-                      ? _controller.pause()
-                      : _controller.play();
-                });
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.stop),
-              onPressed: () {
-                setState(() {
-                  _controller.pause();
-                  _controller.seekTo(Duration.zero); // Возвращаемся к началу
-                });
-              },
-            ),
-          ],
-        ),
-      ],
     );
   }
 }
